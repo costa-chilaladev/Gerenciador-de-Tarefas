c4a50e3c5ff0ff8feae6a8a0fc8e0b036d7a62db
Costa-Chilala-dev <mr.cleaverfox@gmail.com>
2026-05-15 07:23:40 +0100

    task completed-Icon added

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon, CheckIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button.jsx"
 
@@ -17,8 +17,9 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         {tasks.map((task) => (
             <li key={task.id} className="flex gap-2">
                 <button 
-                    className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && "line-through" }`}
+                    className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && "line-through" } flex`}
                     onClick={() => onTaskClick(task.id)}>
+                        {task.isCompleted && <CheckIcon/>}
                         {task.title}
                 </button>
                 
